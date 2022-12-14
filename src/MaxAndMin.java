dee0f38d8971a49c5e0a51c9b5d2487d8ba7cabb
CodeDragon642 <jlejeune642@gmail.com>
2022-12-14 14:09:56 +0000

    Original version

--- src/MaxAndMin.java
+++ src/MaxAndMin.java
@@ -2,11 +2,27 @@ import java.util.Random;
 
 public class MaxAndMin {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int num = rand.nextInt(10) + 1;
-        System.out.println(num);
-
-        int [] numbers = {9, 3, 5, 1, 2, 2, 6, 2, 7, 10};
-
+        Random randomGenerator = new Random();
+        //intialise array1 with 10 numbers
+        int[] numbers = new int [10];
+        //generate 10 random numbers for the array, numbers should be between 1 to 20
+        for (int i=0; i<numbers.length; i++) {
+            numbers[i] = randomGenerator.nextInt(20) + 1;
+            System.out.print(numbers[i] + " ");
+        }
+        //initialise values for the maximum and minimum to be used for comparison
+        int max = numbers[0];
+        int min = numbers[0];
+        //use a for loop to work through the array and if statements to check the value at the index against the max and min
+        for (int i = 0; i<numbers.length; i++){
+            if (numbers[i] > max){
+                max = numbers[i];
+            }
+            else if (numbers[i] < min){
+                min = numbers[i];
+            }
+        }
+        System.out.println(" ");
+        System.out.println("The maximum value in the array is " + max + ". The minimum value in the array is " + min);
     }
 }
